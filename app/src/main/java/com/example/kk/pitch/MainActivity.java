b320697fb2ac8a492927b813ae2c8637c9905d09
Brian Tsai <brian.tsai4@gmail.com>
2019-05-07 18:53:15 -0700

    added profile and groups page

--- app/src/main/java/com/example/kk/pitch/MainActivity.java
+++ app/src/main/java/com/example/kk/pitch/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kk.pitch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        //tab1
+        TabHost.TabSpec spec = tabHost.newTabSpec("Tab one");
+        spec.setContent(R.id.Profile);
+        spec.setIndicator("PROFILE");
+        tabHost.addTab(spec);
+
+        //tab2
+        spec = tabHost.newTabSpec("Tab two");
+        spec.setContent(R.id.Groups);
+        spec.setIndicator("GROUPS");
+        tabHost.addTab(spec);
     }
 }
