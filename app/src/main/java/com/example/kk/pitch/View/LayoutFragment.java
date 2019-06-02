5e64e3e3102391326cb95b8af3bc0fdbca4b5430
Carter Moody <Cartman2490@gmail.com>
2019-06-02 13:08:01 -0700

    started settings drawer

---
+++ app/src/main/java/com/example/kk/pitch/View/LayoutFragment.java
@@ -0,0 +1,19 @@
+package com.example.kk.pitch.View;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LayoutFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
