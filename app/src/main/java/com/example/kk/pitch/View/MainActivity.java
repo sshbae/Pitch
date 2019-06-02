fdd61d6f98449ce55ab66aa727d6ca6c1c88d705
Sarah <sarahbae1998@gmail.com>
2019-06-02 13:04:39 -0700

    added some stuff for drawer layout

--- app/src/main/java/com/example/kk/pitch/View/MainActivity.java
+++ app/src/main/java/com/example/kk/pitch/View/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TabHost;
 import android.widget.TextView;
 
@@ -24,6 +26,8 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends Activity {
 
     private Button sign_out;
+    private ImageButton menu;
+    DrawerLayout.DrawerListener menuListener;
     private Intent login_intent;
     private TextView name_tv;
     private UserInfo uInfo;
@@ -88,6 +92,18 @@ public class MainActivity extends Activity {
 
         name_tv = findViewById(R.id.userName);
         name_tv.setText(name[0]);
+
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        //drawer.addDrawerListener(menuListener);
+
+        menu = findViewById(R.id.hamburgerMenu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*pull out the menu*/
+                drawer.openDrawer(view);
+            }
+        });
 
 
         sign_out = findViewById(R.id.sign_out_button);
